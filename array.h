963f90828e40424372ee5523d479e09952d71cb7
Filip Ignaciuk <250965@edu.p.lodz.pl>
2024-05-22 00:27:08 +0200

    memory allocation

---
+++ array.h
@@ -0,0 +1,14 @@
+#ifndef ARRAY_MEMORY_ALLOCATION_ARRAY_H
+#define ARRAY_MEMORY_ALLOCATION_ARRAY_H
+
+int* enterArray(int *size);
+
+void displayArray(int *array, int size);
+
+void findMaxValue(int *array, int size);
+
+void findMinValue(int *array, int size);
+
+void calculateAverageValue(int *array, int size);
+
+#endif //ARRAY_MEMORY_ALLOCATION_ARRAY_H
